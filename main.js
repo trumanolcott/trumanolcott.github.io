c879c6cdac9e1483fa4d029e0f66c3705bd84e33
trumanolcott <tolc2501@mpsedu.org>
2024-10-01 11:42:44 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const cammel = document.querySelector("#truman");
+
+appendEndless();
+
+function appendEndless() {
+    for (let index = 0; index < 200; index++) {
+        const br = document.createElement("br");
+        truman.append(br);
+        truman.append(`            turman`);
+    }
+}
+
+window.onscroll = function() {
+    // append every 100 pixels
+    if(window.scrollY % 100 == 0) {
+        appendEndless();
+    }
+}
