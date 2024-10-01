79ed303f9125885251967a8c06335cff09b54608
trumanolcott <tolc2501@mpsedu.org>
2024-10-01 12:00:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ function appendEndless() {
     for (let index = 0; index < 200; index++) {
         const br = document.createElement("br");
         truman.append(br);
-        truman.append(`turman`);
+        truman.append(`randomTruman()`);
     }
 }
-
+function randomTruman() {
+    let char = "truman";
+    let result = "";
+    let ran = "";
+    let i = 6;
+    while (i<1) {
+        ran = Math.floor(Math.random()*i);
+        result += char.charAt(ran);
+        char = char.replace(char.charAt(ran),"");
+        i--;
+    }
+}
 window.onscroll = function() {
     // append every 100 pixels
     if(window.scrollY % 100 == 0) {
