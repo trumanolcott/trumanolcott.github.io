75a38d9f73c9f732c0139ff86b6c3e2b198485c9
trumanolcott <tolc2501@mpsedu.org>
2024-10-02 10:58:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,33 @@ const truman = document.querySelector("#truman");
 
 appendEndless();
 function setup() {
-    document.title = randomTruman();
+    document.title = randomTruman() + randomOlcott();
 }
 function appendEndless() {
     for (let index = 0; index < 200; index++) {
         const br = document.createElement("br");
         truman.append(br);
         //createElement(randomTruman());
-        truman.append("", randomTruman());
+        truman.append("", randomTruman() + randomOlcott());
     }
 }
 function randomTruman() {
     let char = "ruman";
     let result = "t";
+    let ran;
+    let i = 5;
+    while (i>0) {
+        ran = Math.floor(Math.random()*i);
+        result += char.charAt(ran);
+        char = char.replace(char.charAt(ran),"");
+        i--;
+    }
+  return result;
+}
+
+function randomOlcott() {
+    let char = "lcott";
+    let result = "o";
     let ran;
     let i = 5;
     while (i>0) {
@@ -31,6 +45,6 @@ window.onscroll = function() {
     //window.scrollY % 100 == 0
     if ((window.innerHeight + Math.round(window.scrollY)) >= (document.body.offsetHeight - 500)) {
         appendEndless();
-        document.title = randomTruman();
+        document.title = randomTruman() + randomOlcott();
     }
 }
